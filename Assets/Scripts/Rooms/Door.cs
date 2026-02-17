4132073e05d85160276b18dc48f32edd80ecb6bd
Mennan Gök <mennan.gok@ug.bilkent.edu.tr>
2026-02-17 08:52:48 +0300

    Fixed the camera repositioning in doors. Now we check in the update method of the door in addition to the ontriggerenter2d method

--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -5,6 +5,23 @@ public class Door : MonoBehaviour
     [SerializeField] private Transform previousRoom;
     [SerializeField] private Transform nextRoom;
     [SerializeField] private CameraController cam;
+    [SerializeField] private GameObject player;
+    private Transform currentRoom;
+
+    private void Update() {
+        if (player.transform.position.x > transform.position.x && currentRoom == previousRoom)
+        {
+            // player is to the right of the door but the current room is set to the previous room, we must be in the next room
+            TransitionRooms(currentRoom, nextRoom);
+            Debug.Log("in update if");
+        }
+        else if (player.transform.position.x < transform.position.x && currentRoom == nextRoom)
+        {
+            // player is to the left of the door but the current room is set to the next room, we must be in the previous room
+            TransitionRooms(currentRoom, previousRoom);
+            Debug.Log("in update else if");
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player")
@@ -12,17 +29,23 @@ public class Door : MonoBehaviour
             if (collision.transform.position.x < transform.position.x)
             {
                 //player coming from the left, so move to the next room
-                cam.MoveToNewRoom(nextRoom);
-                nextRoom.GetComponent<Room>().ActivateRoom(true);
-                previousRoom.GetComponent<Room>().ActivateRoom(false);
+                TransitionRooms(previousRoom, nextRoom);
             }
             else
             {
-                cam.MoveToNewRoom(previousRoom); 
-                previousRoom.GetComponent<Room>().ActivateRoom(true);
-                nextRoom.GetComponent<Room>().ActivateRoom(false);
+                //player coming from the right, so move to the previous room
+                TransitionRooms(nextRoom, previousRoom);
             }
             
         }
+    }
+
+
+    private void TransitionRooms(Transform fromRoom, Transform toRoom)
+    {
+        cam.MoveToNewRoom(toRoom);
+        toRoom.GetComponent<Room>().ActivateRoom(true);
+        fromRoom.GetComponent<Room>().ActivateRoom(false);
+        currentRoom = toRoom;
     }
 }
