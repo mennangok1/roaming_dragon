3089efe98a046b135277c27c165fa25c0fd5e330
Mennan Gök <104532230+mennangok1@users.noreply.github.com>
2026-02-26 09:37:12 +0300

    created new workspace for player strike mechanic

---
+++ Assets/Scripts/Player/PlayerStrike.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerStrike : MonoBehaviour
+{
+
+    [SerializeField] private float flyForce;
+    [SerializeField] private float strikeCooldown;
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }    
+    }
+
+
+
+
+}
